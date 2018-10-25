21681c269dc4898faa5b78945f891783b445e7b0
QOO <p61402@gmail.com>
2018-10-25 20:39:05 +0800

    Implement function bang

--- bits.c
+++ bits.c
@@ -206,7 +206,12 @@ int anyOddBit(int x)
  */
 int bang(int x)
 {
-    return 42;
+    x |= x >> 16;
+    x |= x >> 8;
+    x |= x >> 4;
+    x |= x >> 2;
+    x |= x >> 1;
+    return (x ^ 1) & 1;
 }
 
 /*
