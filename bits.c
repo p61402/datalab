b56600bdfd44bba2a8e15cd9ed1075d3be76da6c
QOO <p61402@gmail.com>
2018-10-27 12:19:20 +0800

    Implement function bitParity

--- bits.c
+++ bits.c
@@ -306,7 +306,12 @@ int bitOr(int x, int y)
  */
 int bitParity(int x)
 {
-    return 42;
+    x ^= x >> 16;
+    x ^= x >> 8;
+    x ^= x >> 4;
+    x ^= x >> 2;
+    x ^= x >> 1;
+    return x & 1;
 }
 
 /*
